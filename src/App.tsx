12db37c35bb54b0346a82d98d7af94034ad66bb8
Cleiton Mariano <cleitonomariano@gmail.com>
2021-06-01 20:24:04 -0300

    create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyle from './Styles/GlobalStyle';
 
 const App: React.FC = () => (
-  <Router>
-    <Routes />
-  </Router>
+  <>
+    <Router>
+      <Routes />
+    </Router>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
