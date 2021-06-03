3f9a3b22421545473fce9b94f8e690734e274154
Cleiton Mariano <cleitonomariano@gmail.com>
2021-06-03 18:17:54 -0300

    connect with firebase

---
+++ src/DataBase/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCNBDLhM1dQ_xazDuxqMj6wJCsAUbq7Thw',
+  authDomain: 'sponte-chellenge.firebaseapp.com',
+  projectId: 'sponte-chellenge',
+  storageBucket: 'sponte-chellenge.appspot.com',
+  messagingSenderId: '571732822722',
+  appId: '1:571732822722:web:89929ac11e770d4b88895b',
+};
+// Initialize Firebase
+// eslint-disable-next-line no-undef
+firebase.initializeApp(firebaseConfig);
--- src/Pages/RegisterProduct/index.tsx
+++ src/Pages/RegisterProduct/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Button from '../../Components/Button';
+import Container from '../../Components/Container';
 
 const RegisterProduct: React.FC = () => (
   <>
-    <h1>RegisterProduct</h1>
-    <form>
-      <Button>Salvar</Button>
-    </form>
+    <Container>
+      <h1>RegisterProduct</h1>
+      <form>
+        <Button>Salvar</Button>
+      </form>
+    </Container>
   </>
 );
 
